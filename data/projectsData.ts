fbc878e8f29a8f74ff5d9306a2da41da561f0ad4
Dov Alperin <git@dov.dev>
2022-01-08 14:07:11 -0500

    Fix yjs project description

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -17,9 +17,7 @@ const projectsData = [
   // },
   {
     title: 'Bringing subdocuments to YJS',
-    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-     to the desired date and press "Go". No more worrying about lost keys or
-     forgotten headphones with this simple yet affordable solution.`,
+    description: `Bringing native subdocument support to the YJS ecosystem.`,
     imgSrc: '/static/images/yjs.svg',
     href: '/blog/the-time-machine',
   },
